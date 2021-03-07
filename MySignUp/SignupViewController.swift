fb80ccc65ea7d13daf1154442cc28b0fe73a8e45
seunghun yang <yabby1997@gmail.com>
2021-03-07 17:36:21 +0900

    View 수정

---
+++ MySignUp/SignupViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SignupViewController.swift
+//  MySignUp
+//
+//  Created by Seunghun Yang on 2021/03/07.
+//
+
+import UIKit
+
+class SignupViewController: ViewController {
+
+    @IBOutlet weak var pofileImageView: UIImageView!
+    @IBOutlet weak var idField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordCheckField: UITextField!
+    @IBOutlet weak var bioTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+    }
+}
