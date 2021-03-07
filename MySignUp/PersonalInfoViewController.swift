5bc4ff4a8c78953227c375c2d8e775510ed5ad53
seunghun yang <yabby1997@gmail.com>
2021-03-07 18:11:08 +0900

    프로필 사진으로 원본이미지 선택 가능하게 구현

---
+++ MySignUp/PersonalInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PersonalInfoViewController.swift
+//  MySignUp
+//
+//  Created by Seunghun Yang on 2021/03/07.
+//
+
+import UIKit
+
+class PersonalInfoViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MySignUp/SignupViewController.swift
+++ MySignUp/SignupViewController.swift
@@ -7,21 +7,40 @@
 
 import UIKit
 
-class SignupViewController: ViewController {
+class SignupViewController: ViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    @IBOutlet weak var pofileImageView: UIImageView!
+    @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var idField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var passwordCheckField: UITextField!
     @IBOutlet weak var bioTextView: UITextView!
     
+    lazy var imagePicker: UIImagePickerController = {
+        let picker: UIImagePickerController = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        return picker
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let profileImageTappGesture = UITapGestureRecognizer(target: self, action: #selector(self.imagePickerTapped(_:)))
+        profileImageView.isUserInteractionEnabled = true
+        profileImageView.addGestureRecognizer(profileImageTappGesture)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        self.profileImageView.image = image
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func imagePickerTapped(_ sender: Any) {
+        self.present(self.imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func cancelButtonTapped(_ sender: Any) {
-    }
-    
-    @IBAction func nextButtonTapped(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
     }
 }
