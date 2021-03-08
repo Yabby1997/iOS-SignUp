c508ca8ba55c532dd21d8c342ed27497462d227a
seunghun yang <yabby1997@gmail.com>
2021-03-08 13:50:21 +0900

    회원가입시 ID TextField에 가입한 ID 기입되도록 구현

--- MySignUp/ViewController.swift
+++ MySignUp/ViewController.swift
@@ -9,7 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var signInIDField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if self.signInIDField != nil {
+            if let idString = UserInformation.sharedUserInformation.id {
+                self.signInIDField.text = idString
+            } else {
+                self.signInIDField.text = ""
+            }
+        }
     }
 }
